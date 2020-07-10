33fd6a384612e284089fe3c5924c13fdd0793fb3
CloudChaoszero <rmaldonadocloud@outlook.com>
2020-07-10 03:32:09 -0700

    :wrench: run configuration updates GEm for latest configuration Index.html changes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,54 +3,74 @@ var store = [{
         "excerpt":"I wrote this when I was a Teller at Wells Fargo in 2016. I thought I’d share it to inspire other aspired individuals to reach not their dreams, but their goals. “Setting goals is the first step in turning the invisible into the visible.” — Tony Robbins My activities in...","categories": [],
         "tags": ["Motivation"],
         "url": "http://localhost:4000/i-wrote-this-when-i-was-a-teller-at-wells-fargo-in-2016/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Opening your HTML code through VS Code",
         "excerpt":"Being a past Sublime PC user, I would right click the html file and select Open in Browser. Pretty simple. However, now I would like to go through this process in VS Code. How would I open HTML code from VS Code into my web browser, particularly Google Chrome? Well,...","categories": [],
         "tags": ["Programming"],
         "url": "http://localhost:4000/opening-your-html-code-through-vs-code/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Farewell, MobileIron",
         "excerpt":"Today was my last day contracting for MobileIron. I was onboarded in August 2017 as the Business Application team’s Data Analyst for an ongoing project. What has felt like such a small time-frame, almost one year, I am humbled to have had this great opportunity to work with a wonderful...","categories": [],
         "tags": ["Career"],
         "url": "http://localhost:4000/farewell-mobileiron/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Resources for creating Tableau Visualizations through Python",
         "excerpt":"I have not personally tried to create Tableau visualizations through Python, but there does exist a python library called TabPy. This library is currently being developed by the Tableau team, and is under Beta/development. tableau/TabPy Execute Python code on the fly and display results in Tableau visualizations - tableau/TabPy TabPy...","categories": [],
         "tags": ["Visualization"],
         "url": "http://localhost:4000/resources-for-creating-tableau-visualizations-through-python/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Quickly Getting Started with Google Maps API w/Python (July 2018 Platform)",
         "excerpt":"You probably want to harness the power of Google Maps API for application development. Moreover, your are coding in Python. This article provides instruction on how to utilize Google Map’s API from the new Maps Platform, and a brief mention about the legacy process ( Post July 2018 and Pre-July...","categories": [],
         "tags": ["Coding"],
         "url": "http://localhost:4000/quickly-getting-started-with-google-maps-api-w-python-july-2018-platform/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Finding Data Professional Mentors or Meetups in San Francisco Bay Area",
         "excerpt":"Though I never had the opportunity to be a part of, you either just finished your Data Analytics/Science bootcamp or class. #BelatedCongrats! Or maybe you are just part of the following: Career change into the Data profession Recent college graduate Forgotten what places to go to to network Well, don’t...","categories": [],
         "tags": ["Networking"],
         "url": "http://localhost:4000/finding-data-professional-mentors-or-meetups-in-san-francisco-bay-area/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Next Career Move, Striving for Full-Time Contributing Analytic and Technical Solutions",
         "excerpt":"It’s been 1 week since I started the next part of my journey — a situation in which a lot of my coworkers appeared startled after responding “Nothing yet. I am going to start looking for my next opportunity soon” after the question “Where are you going to go next?”…...","categories": [],
         "tags": ["Career"],
         "url": "http://localhost:4000/next-career-move-striving-for-full-time-contributing-analytic-and-technical-solutions/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Quick Guide to OKRs",
         "excerpt":"What is an OKR? OKR (Objectives and Key Results)[Not OKRA] is a framework for defining and tracking objectives and outcomes to drive engagement and focus on your goals. Objectives are motivational, challenging, and qualitative descriptions of what you want to achieve. **Key Results **are a set of metrics that measure...","categories": [],
         "tags": ["Project Management"],
         "url": "http://localhost:4000/quick-guide-to-okrs/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "SaaS Metrics",
         "excerpt":"This document is a review and breakdown of Metrics Definitions and formulas related to SaaS Metrics, particulary 16 SaaS Metrics critical for business growth Breaking Down Profit Profit = Revenue - Cost Where: Revenue = Paying Users x amount paid by userrevenue is equal to Paying Users = New Users...","categories": [],
         "tags": ["Project Management"],
         "url": "http://localhost:4000/SaaS-Metrics/",
-        "teaser":null},{
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
+        "title": "Why I Upgraded my Website from HTML -> Jekyll",
+        "excerpt":"My initial blog started off with several considerations as a website. Should I create a website? If I create the website, how much would I have to pay for a yearly subscription? Is it worth it? What about free solutions? I tried it out, and it didn’t work out. I...","categories": [],
+        "tags": ["Coding"],
+        "url": "http://localhost:4000/Migrating-Blog/",
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
         "title": "Survival Analysis",
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Survival-Analysis/",
-        "teaser":null},{
-        "title": "2019 Lookback | \\#2020Vision Outlook",
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
+        "title": "2019 Lookback and the \\#2020Vision",
         "excerpt":"It’s been a while since I’ve produced some quality personal content. Moreover, I’ve noticed that it’s been 1 year since my time as a Data Analyst at Autodesk! Though the journey has been amazing to work in a start-up-like workflow, meanwhile working to drive impact to a corporation, I thought...","categories": [],
         "tags": ["Motivation"],
         "url": "http://localhost:4000/2019Lookback-2020Growth/",
-        "teaser":null}]
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
+        "title": "Joining \\#Masks4All against Coronavirus",
+        "excerpt":"I am a avid viewer of Lex Fridman’s podcasts and other misc. content, particularly his Youtube videos. I recently watched one of his videos on “Face Masks for All” &amp; DIY Masks, seen here. In it, he discusses what you need to know about face masks and its impact on...","categories": [],
+        "tags": ["Personal"],
+        "url": "http://localhost:4000/Joining-Masks4All/",
+        "teaser":"http://localhost:4000/assets/images/covid19/effect-against-particles.png"},{
+        "title": "Timeseriesanalysis Overview",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/TimeSeriesAnalysis-Overview/",
+        "teaser":"http://localhost:4000/assets/images/ProfilePicture.jpg"},{
+        "title": "SQL Readability (2020)",
+        "excerpt":"Scenario: You submitted a Pull Request (PR) for a sophisticated contribution to production (prod), that you spent several hours working on. Kudos to you! But then a code reviewer does not approve your PR. Your query worked, so what happened? It may turn out that you’re writing style may not...","categories": [],
+        "tags": ["Coding"],
+        "url": "http://localhost:4000/SQL-Readability/",
+        "teaser":"http://localhost:4000/assets/images/sql-query-worked.jpg"}]
